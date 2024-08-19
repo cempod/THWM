d0b02735ed0800778cfcbdef0661525367efd8a1
cempod <cempod@gmail.com>
2024-08-19 19:20:50 +0300

    Fix test blink. It works!

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -9,16 +9,20 @@ main(void) {
   
   /*for test*/
   LL_GPIO_InitTypeDef gpio_init = {0};
-    LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOC);
+    LL_AHB4_GRP1_EnableClock(LL_AHB4_GRP1_PERIPH_GPIOE);
     gpio_init.Pin = LL_GPIO_PIN_3;
     gpio_init.Mode = LL_GPIO_MODE_OUTPUT;
     gpio_init.Speed = LL_GPIO_SPEED_FREQ_HIGH;
     gpio_init.OutputType = LL_GPIO_OUTPUT_PUSHPULL;
     gpio_init.Alternate =  LL_GPIO_AF_0;
-    LL_GPIO_Init(GPIOC, &gpio_init);
+    LL_GPIO_Init(GPIOE, &gpio_init);
   while (1) {
-    LL_GPIO_TogglePin(GPIOC, LL_GPIO_PIN_3);
-    LL_mDelay(250);
+    LL_GPIO_TogglePin(GPIOE, LL_GPIO_PIN_3);
+    LL_mDelay(500);
   }
   
 }
+
+void SVC_Handler(){}
+void PendSV_Handler(){}
+void SysTick_Handler(){}
