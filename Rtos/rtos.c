de3d2bb1291823ccde119d34850176b48185a824
cempod <cempod@gmail.com>
2024-09-10 14:39:33 +0300

    We need more memory

--- Rtos/rtos.c
+++ Rtos/rtos.c
@@ -7,7 +7,7 @@ TaskHandle_t backlight_task_handle;
 
 void
 threads_init(void) {
-    xTaskCreate(display_task, "display task", configMINIMAL_STACK_SIZE*5, NULL, configMAX_PRIORITIES - 1, &display_task_handle);
+    xTaskCreate(display_task, "display task", configMINIMAL_STACK_SIZE*10, NULL, configMAX_PRIORITIES - 1, &display_task_handle);
     xTaskCreate(backlight_task, "backlight task", configMINIMAL_STACK_SIZE*1, NULL, configMAX_PRIORITIES - 1, &backlight_task_handle);
 }
 
